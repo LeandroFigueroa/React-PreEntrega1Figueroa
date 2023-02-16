9b4db78d03ac920247aa5ca81e125da10681e842
LeandroFigueroa <c.leandrofigueroa@gmail.com>
2023-02-16 15:24:59 -0300

    4nd commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,30 @@
 import React from "react";
 import "./styles/navbar.css";
-import CartWidget from './CartWidget';
+import CartWidget from "./CartWidget";
 
 const NavBar = (props) => {
   return (
-
     <header>
- 
-    <nav className="nav">
-      <a href="/" className="site-title">
-        MercadoVago
-      </a>
-      <ul>
-        <li className="active">
-          <a href="/productos"> Productos</a>
-        </li>
-        <li>
-          <a href="/nosotros"> Nosotros </a>
-        </li>
-        <li>
-        
-          <a href="/carrito"> <CartWidget /> </a>
-          
-        </li>
-      </ul>
-
-
-    </nav>
-
-    
-
+      <nav className="nav">
+        <a href="/" className="site-title">
+          MercadoVago
+        </a>
+        <ul>
+          <li className="active">
+            <a href="/productos"> Productos</a>
+          </li>
+          <li>
+            <a href="/nosotros"> Nosotros </a>
+          </li>
+          <li>
+            <a href="/carrito">
+              {" "}
+              <CartWidget />{" "}
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
-
   );
 };
 
